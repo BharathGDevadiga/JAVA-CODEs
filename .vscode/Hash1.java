f133f7f281e9a81bb8f8e720acf7d4c64cef5e14
BharathGDevadiga <myasuslaptop45@gmail.com>
2026-02-26 15:36:51 +0530

    Hash Learn

---
+++ .vscode/Hash1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Hash1{
+    public static void main(String[] args)
+    {
+        HashMap<String,Integer>map=new HashMap<>();
+        map.put("English",85);
+        map.put("Maths",90);
+        map.put("Science",95);
+        System.out.println(map.containsKey("English"));
+        System.out.println(map.containsValue(90));
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        
+
+    for(Map.Entry<String,Integer>entry:map.entrySet()){
+        if(entry.getValue()>=90){
+            System.out.println(entry.getKey()+" ");
+        }
+    }
+}
+}

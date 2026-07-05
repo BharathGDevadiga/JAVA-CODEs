7d56790d09c6546201f36c88a8f507bfa5fbcac8
bharath <myasuslaptop45@gmail.com>
2026-07-05 15:31:08 +0530

    Update and rename BinaryTreesMain.java to Trees/BinaryTreeTraversals.java

--- Trees/BinaryTreeTraversals.java
+++ Trees/BinaryTreeTraversals.java
@@ -1,47 +1,66 @@
 import java.util.*;
 
-class Node {
+class BinaryTreeTraversals{
+    
     int data;
     Node left, right;
     
     Node(int d) {
+        
         data = d;
         left = right = null;
+        
     }
 }
 
 class BinaryTrees {
+    
     Node root;
     
     // Constructor to initialize root
     BinaryTrees() {
+        
         root = null;
+        
     }
 
     void insert(int d) {
+        
         Node new_node = new Node(d);
         if (root == null) {
+            
             root = new_node;
             return;
+            
         }
+        
         Queue<Node> q = new LinkedList<>();
         q.add(root);
 
         while (!q.isEmpty()) {
+            
             Node temp = q.poll();
 
             if (temp.left == null) {
+                
                 temp.left = new_node;
                 return;
+                
             } else {
+                
                 q.add(temp.left);
+                
             }
 
             if (temp.right == null) {
+                
                 temp.right = new_node;
                 return;
+                
             } else {
+                
                 q.add(temp.right);
+                
             }
         }
     }
@@ -51,26 +70,36 @@ class BinaryTrees {
         inOrder(temp.left);
         System.out.print(temp.data + " ");
         inOrder(temp.right);
+        
     }
 
     void preOrder(Node temp) {
         if (temp == null) return;
         System.out.print(temp.data + " ");
+        
         preOrder(temp.left);
         preOrder(temp.right);
+        
     }
 
     void postOrder(Node temp) {
+        
         if (temp == null) return;
+        
         postOrder(temp.left);
         postOrder(temp.right);
+        
         System.out.print(temp.data + " ");
+        
     }
 }
 
 public class BinaryTreesMain {
+    
     public static void main(String[] args) {
+        
         BinaryTrees tree = new BinaryTrees();
+        
         tree.insert(1);
         tree.insert(2);
         tree.insert(3);
@@ -81,9 +110,12 @@ public class BinaryTreesMain {
 
         System.out.println("InOrder:");
         tree.inOrder(tree.root);
+        
         System.out.println("\nPreOrder:");
         tree.preOrder(tree.root);
+        
         System.out.println("\nPostOrder:");
         tree.postOrder(tree.root);
+        
     }
 }

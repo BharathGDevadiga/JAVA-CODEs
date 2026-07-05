eb971de56e8d0344d4015ca8e2b764be8f54c486
bharath <myasuslaptop45@gmail.com>
2026-07-05 15:16:00 +0530

    Update and rename Pla/proj1/src/in/smvitm/ecea/main/Main.java to Pla/proj1/src/main/Main.java

--- Pla/proj1/src/main/Main.java
+++ Pla/proj1/src/main/Main.java
@@ -5,11 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import in.smvitm.ecea.beans.Student;
 
 public class Main {
+    
     public static void main(String[] args) {
 
         String path="in/smvitm/ecea/resources/applicationContext.xml";
             ApplicationContext container = new ClassPathXmlApplicationContext(path);
          Student std = (Student) container.getBean("student");
+        
          std.display();
+        
     }
 }

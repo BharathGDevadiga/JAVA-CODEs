12486c156443cf8342ec1cc298a8a1a129e5c796
bharath <myasuslaptop45@gmail.com>
2026-07-05 15:32:31 +0530

    Update and rename FactorialRecursion.java to Recursion/FactorialRecursion.java

--- Recursion/FactorialRecursion.java
+++ Recursion/FactorialRecursion.java
@@ -9,9 +9,11 @@ public class FactorialRecursion{
         return n*fact(n-1);
         
     }
+    
     public static void main(String[] args){
         
         Scanner sc=new Scanner(System.in);
+        
         int n=sc.nextInt();
         System.out.print(fact(n));
         

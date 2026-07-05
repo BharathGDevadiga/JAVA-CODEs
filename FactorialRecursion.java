bb7885b7e1a17a4aa6b4aea97c899143559e33ec
bharath <myasuslaptop45@gmail.com>
2026-07-05 14:59:03 +0530

    Update and rename Recur.java to FactorialRecursion.java

--- FactorialRecursion.java
+++ FactorialRecursion.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
-public class Recur {
+
+public class FactorialRecursion{
+    
     public static int fact(int n){
+        
         if(n==0)
             return 1;
         return n*fact(n-1);
+        
     }
     public static void main(String[] args){
+        
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         System.out.print(fact(n));
+        
     }
 }

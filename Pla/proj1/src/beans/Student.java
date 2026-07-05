abaf787ee87eba12adaa90e7544e832bb7886623
bharath <myasuslaptop45@gmail.com>
2026-07-05 15:16:58 +0530

    Update and rename Pla/proj1/src/in/smvitm/ecea/beans/Student.java to Pla/proj1/src/beans/Student.java

--- Pla/proj1/src/beans/Student.java
+++ Pla/proj1/src/beans/Student.java
@@ -1,37 +1,51 @@
 package in.smvitm.ecea.beans;
 
 public class Student {
+    
     private String name;
     private int age;
     private String dept;
         
     public String getName() {
+        
         return this.name;
+        
     }
 
     public void setName(String name) {
+        
         this.name = name;
+        
     }
 
     public int getAge() {
+        
         return this.age;
+        
     }
 
     public void setAge(int age) {
+        
         this.age = age;
     }
 
     public String getDept() {
+        
         return this.dept;
+        
     }
 
     public void setDept(String dept) {
+        
         this.dept = dept;
+        
     }
 
     public void display(){
+        
             System.out.println("Name: "+this.name);
             System.out.println("Age: "+this.age);
             System.out.println("Department: "+this.dept);
+        
         }
 }
